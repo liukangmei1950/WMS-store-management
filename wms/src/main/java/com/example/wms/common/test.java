3648b79029f5713b4d1f529dbca2862c554dc2c9
liukangmei1950 <2942324212@qq.com>
2024-02-21 16:05:04 +0800

    完成ces

--- wms/src/main/java/com/example/wms/common/test.java
+++ wms/src/main/java/com/example/wms/common/test.java
@@ -2,6 +2,6 @@ package com.example.wms.common;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("testssss");
     }
 }
